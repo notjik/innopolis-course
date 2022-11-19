bc90d203149328d4087853caf7e500b181eaea90
Pavel <notjik@yandex.ru>
2022-11-19 23:03:45 +0500

    add comments

--- 141122/cw/2.py
+++ 141122/cw/2.py
@@ -1,44 +1,44 @@
-x = 5
-if x >= 5:
-    print('YES')
-if x == 5:
-    print('YES')
-if x != 10:
-    print('YES')
-else:
-    print('NO')
+# x = 5
+# if x >= 5:
+#     print('YES')
+# if x == 5:
+#     print('YES')
+# if x != 10:
+#     print('YES')
+# else:
+#     print('NO')
 
-age = int(input('Введите свой возраст: '))
-if age >= 14:
-    print('Вам больше  14 или 14 лет')
-elif age >= 18:
-    print('Вам больше или 18 лет')
-elif age >= 21:
-    print('Вам больше или 21 год')
-elif age > 45:
-    print('Вам больше 45 лет')
+# age = int(input('Введите свой возраст: '))
+# if age >= 14:
+#     print('Вам больше  14 или 14 лет')
+# elif age >= 18:
+#     print('Вам больше или 18 лет')
+# elif age >= 21:
+#     print('Вам больше или 21 год')
+# elif age > 45:
+#     print('Вам больше 45 лет')
 
-age = 12
-if age > 11:
-    if age < 15:
-        print('Да')
+# age = 12
+# if age > 11:
+#     if age < 15:
+#         print('Да')
 
-age = 12
-if age > 11 and age < 15:
-    print('Да')
+# age = 12
+# if age > 11 and age < 15:
+#     print('Да')
 
-age = 12
-if 11 < age < 15:
-    print('Да')
+# age = 12
+# if 11 < age < 15:
+#     print('Да')
 
-num = int(input('Введите число'))
-print('Четное' if num % 2 == 0 else 'Нечетное')
+# num = int(input('Введите число'))
+# print('Четное' if num % 2 == 0 else 'Нечетное')
 
-num = int(input())
-if num > 100 or num < -100:
-    print('-')
-else:
-    print('+')
+# num = int(input())
+# if num > 100 or num < -100:
+#     print('-')
+# else:
+#     print('+')
 
 num1 = int(input())
 num2 = int(input())
