7022dab8a9aebaa1fec578837500f71bb135d3dd
Pavel <notjik@yandex.ru>
2022-11-19 23:00:49 +0500

    new classwork + adjustment

---
+++ 141122/cw/1.py
@@ -0,0 +1,39 @@
+# x = 5
+# if x == 5:
+#     print('x равно')
+# else:
+#     print('x не равно')
+
+# num1 = 3
+# num2 = 7
+# if num1 < num2:
+#     print('Меньше')
+# else:
+#     print('Больше')
+
+# x = 5
+# if x > 2:
+#     if x < 10:
+#         print('x больше 2, но меньше 10')
+
+# age = 12
+# user_class = 7
+# if age >= 12 and user_class == 7:
+#     print('Условие верно')
+
+# age = 12
+# user_class = 7
+# if age >= 12 or user_class == 7:
+#     print('Условие верно')
+
+# age = 12
+# if not age == 12:
+#     print('Возраст не равен 12')
+
+temp = float(input('Введите температуру '))
+if temp <= 36.6:
+    print('Здоров')
+if temp <= 37.5:
+    print('Низкая температура')
+if temp > 38:
+    print('Высокая температура')
