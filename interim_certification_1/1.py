8b1e486e4d005428e6354a30440fa8ad0ef27354
Pavel <notjik@yandex.ru>
2022-11-27 00:03:47 +0500

    completed the interim certification

---
+++ interim_certification_1/1.py
@@ -0,0 +1,13 @@
+"""
+Реализовать программу, которая принимает на вход числа a, b.
+
+Результатом работы должно быть среднее значение всех чисел от a до b включительно, которые делятся на 3,
+среднее значение находится, как сумма чисел разделенное на количество.
+"""
+tmp = list(map(lambda x: x.strip(), input('Введите два числа через пробел: ').split()))[:2]
+while not (tmp[0].isdecimal() or tmp[1].isdecimal()):
+    tmp = list(map(lambda x: x.strip(), input('Введите два числа через пробел: ').split()))[:2]
+a, b = map(int, tmp)
+lst = [i for i in range(int(tmp[0]), int(tmp[1]) + 1) if not (i % 3)]
+avg = sum(lst) / len(lst)
+print(f'Среднее значение: {avg}')
