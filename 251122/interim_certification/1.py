a6c773710ada3cdf75958631d25a583a72085ede
Pavel <notjik@yandex.ru>
2022-11-27 11:44:13 +0500

    small fix

--- 251122/interim_certification/1.py
+++ 251122/interim_certification/1.py
@@ -4,10 +4,15 @@
 Результатом работы должно быть среднее значение всех чисел от a до b включительно, которые делятся на 3,
 среднее значение находится, как сумма чисел разделенное на количество.
 """
-tmp = list(map(lambda x: x.strip(), input('Введите два числа через пробел: ').split()))[:2]
-while not (tmp[0].isdecimal() or tmp[1].isdecimal()):
+while True:
     tmp = list(map(lambda x: x.strip(), input('Введите два числа через пробел: ').split()))[:2]
-a, b = map(int, tmp)
-lst = [i for i in range(int(tmp[0]), int(tmp[1]) + 1) if not (i % 3)]
-avg = sum(lst) / len(lst)
-print(f'Среднее значение: {avg}')
+    while not (tmp[0].isdecimal() or tmp[-1].isdecimal()):
+        tmp = list(map(lambda x: x.strip(), input('Введите два числа через пробел: ').split()))[:2]
+    a, b = map(int, tmp)
+    lst = [i for i in range(int(tmp[0]), int(tmp[-1]) + 1) if not (i % 3)]
+    try:
+        avg = sum(lst) / len(lst)
+        print(f'Среднее значение: {avg}')
+        break
+    except ZeroDivisionError as f:
+        print(f'В выбранном промежутке нет чисел, делящихся на три.\nОшибка: {f}\n\n')
