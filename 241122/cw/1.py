854be164d5082b06459a232f3d3d1985ad56cc04
Pavel <notjik@yandex.ru>
2022-12-02 21:50:54 +0500

    new classwork + homework

---
+++ 241122/cw/1.py
@@ -0,0 +1,18 @@
+"""↓ collaborations ↓"""
+# numbers = [2, 4, 6, 8, 10]
+# languages = ['Python', 'C#', 'C++', 'Java']
+# print(numbers)
+# print(languages)
+
+# even_numbers = list(range(0, 10, 2))
+# odd_numbers = list(range(1, 10, 2))
+
+# s = 'abcde'
+# chars = list(s)
+
+# numbers = [[1, 2, 3], [4, 5, 6]]
+# print(numbers[0][2])
+
+"""↓ personal work ↓"""
+# a = [0, 1, 2, 3, 4, 5]
+# a[1:3] = 6 (TypeError)
---
+++ 241122/cw/2.py
@@ -0,0 +1,58 @@
+"""↓ collaborations ↓"""
+# a = [1, 2, 3]
+# a.append(4)
+# print(a)
+
+# a = [1, 2, 3]
+# a.remove(2)
+# print(a)
+
+# a = [1, 2, 3]
+# b = a.pop(2)
+# print(a)
+# print(b)
+
+# a = [1, 2, 3]
+# a.clear()
+# print(a)
+
+# a = [2, 1, 3]
+# a.sort()
+# print(a)
+
+# a = [1, 2, 3]
+# b = [3, 5, 6]
+# a.extend(b)
+# print(a)
+
+# a = [2, 1, 3]
+# print(a.index(3))
+
+# a = [2, 1, 3]
+# a.insert(0, 4)
+# print(a)
+
+# a = (1, 2)
+# a = list(a)
+# a.append(3)
+# a = tuple(a)
+# print(a)
+
+# a = []
+# i = 0
+# while len(a) < 45:
+#     if i % 2 == 0:
+#         a.append(i)
+#     i += 1
+# print(a)
+
+
+"""↓ personal work ↓"""
+a = []
+i = 0
+while len(a) != 45:
+    if not(i % 2):
+        a.append(i)
+    i += 1
+print(a)
+
